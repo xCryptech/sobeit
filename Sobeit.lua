450c6032f7fb2185db9d1dc7106719d5577ac7ab
xCryptech <42791075+xCryptech@users.noreply.github.com>
2018-10-28 04:49:35 +0000

    A small fixes

--- Sobeit.lua
+++ Sobeit.lua
@@ -1,11 +1,11 @@
 menu = gg.choice({
 	"1. GOD-MODE [ON]",
 	"2. GOD-MODE [OFF]",
-	"3. Speed-Hack [ON]",
-	"4. Speed-Hack [OFF]",
-	"5. Suicide (BETA)",
-	"6. Cam-Hack [ON]",
-	"7. Cam-Hack [OFF]",
+	"3. SPEED-HACK [ON]",
+	"4. SPEED-HACK [OFF]",
+	"5. SUICIDE (BETA)",
+	"6. CAM-HACK [ON]",
+	"7. CAM-HACK [OFF]",
 },nil,"▶ Sobeit by xCryptech ◀")
 
 if menu == 1 then
@@ -39,7 +39,7 @@ gg.searchNumber('100', gg.TYPE_FLOAT)
 gg.getResults(2500)
 gg.editAll('0.1', gg.TYPE_FLOAT)
 gg.clearResults()
-print('▶ Suicide Activated!')
+print('▶ SUICIDE Activated!')
 end
 
 if menu == 6 then
@@ -47,7 +47,7 @@ gg.searchNumber('3.59999990463', gg.TYPE_FLOAT)
 gg.getResults(2500)
 gg.editAll('134.5613', gg.TYPE_FLOAT)
 gg.clearResults()
-print('▶ CamHack Activated!');
+print('▶ CAM-HACK Activated!');
 end
 
 if menu == 7 then
@@ -55,6 +55,6 @@ gg.searchNumber('134.5613', gg.TYPE_FLOAT)
 gg.getResults(2500)
 gg.editAll('3.59999990463', gg.TYPE_FLOAT)
 gg.clearResults()
-print('▶ CamHack Deactivated!');
+print('▶ CAM-HACK Deactivated!');
 end
 

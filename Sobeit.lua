88330fe2cbad1c2ca813fed4e91087de78fdf836
xCryptech <42791075+xCryptech@users.noreply.github.com>
2018-10-29 02:14:00 +0000

    Added cheat 'GRAVITY-FUCKER'.
    Have a fun :)

--- Sobeit.lua
+++ Sobeit.lua
@@ -6,6 +6,8 @@ menu = gg.choice({
 	"5. SUICIDE (BETA)",
 	"6. CAM-HACK [ON]",
 	"7. CAM-HACK [OFF]",
+	"8. GRAVITY-FUCKER [ON]",
+	"9. GRAVITY-FUCKER [OFF]",
 },nil,"▶ Sobeit by xCryptech ◀")
 
 if menu == 1 then
@@ -47,7 +49,7 @@ gg.searchNumber('3.59999990463', gg.TYPE_FLOAT)
 gg.getResults(2500)
 gg.editAll('134.5613', gg.TYPE_FLOAT)
 gg.clearResults()
-print('▶ CAM-HACK Activated!');
+print('▶ CAM-HACK Activated!')
 end
 
 if menu == 7 then
@@ -55,6 +57,21 @@ gg.searchNumber('134.5613', gg.TYPE_FLOAT)
 gg.getResults(2500)
 gg.editAll('3.59999990463', gg.TYPE_FLOAT)
 gg.clearResults()
-print('▶ CAM-HACK Deactivated!');
+print('▶ CAM-HACK Deactivated!')
 end
 
+if menu == 8 then
+gg.searchNumber('0.008', gg.TYPE_FLOAT)
+gg.getResults(2500)
+gg.editAll('0.0002', gg.TYPE_FLOAT)
+gg.clearResults()
+print('▶ GRAVITY-FUCKER Activated!');
+end
+
+if menu == 9 then
+gg.searchNumber('0.0002', gg.TYPE_FLOAT)
+gg.getResults(2500)
+gg.editAll('0.008', gg.TYPE_FLOAT)
+gg.clearResults()
+print('▶ GRAVITY-FUCKER Deactivated!');
+end
